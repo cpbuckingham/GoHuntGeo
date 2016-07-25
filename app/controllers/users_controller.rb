fbdbd586820999fb220286553c95bddb624b80af
Cam <cameron.p.buckingham@gmail.com>
2016-07-25 16:42:14 -0500

    WIP

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to dashboard_path
+      redirect_to user_path
     else
       render "new"
     end
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,6 @@ Rails.application.routes.draw do
   get "sign_up" => "users#new", :as => "sign_up"
 
   resources :users
-  resources :dashboard
-
 
 
 end
